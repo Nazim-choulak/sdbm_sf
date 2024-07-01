e1ef8094cf52640d71c8d397be1465fed65e9afd
Nazim CHOULAK <Choulak.nazim@gmail.com>
2024-07-01 16:38:49 +0200

    Controller/Entity/CRUD-Pays

---
+++ src/Entity/Pays.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PaysRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PaysRepository::class)]
+class Pays
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $nom = null;
+
+    #[ORM\ManyToOne(inversedBy: 'pays')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Continent $continents = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getContinents(): ?Continent
+    {
+        return $this->continents;
+    }
+
+    public function setContinents(?Continent $continents): static
+    {
+        $this->continents = $continents;
+
+        return $this;
+    }
+}
